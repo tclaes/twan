69dce18f4f78bcf60adb5620e28416a7b57b03a1
Tom Claes <tom.claes82@gmail.com>
2020-12-11 00:54:27 +0100

    Feature/vue router (#4)
    * router config added
    
    * testing added
    
    * pre commit hooks
    
    * pre commit hooks

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: '@vue/cli-plugin-unit-jest',
+  collectCoverage: true,
+
+  collectCoverageFrom: [
+    "src/**/*.{js,vue}",
+    "!**/node_modules/**"
+  ],
+
+  coverageReporters: [
+    "html",
+    "text-summary"
+  ],
+
+  transform: {
+    '^.+\\.vue$': 'vue-jest'
+  }
+}
