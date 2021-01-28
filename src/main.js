558cae81e4374085ab475e4cc414868af8829ba4
Tom Claes <tomclaes@Toms-MacBook-Air.local>
2021-01-28 22:00:26 +0100

    fix analytics

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import App from "./App.vue";
 import './assets/css/styles.css';
 import './registerServiceWorker';
 import router from './router';
+import VueGtag from "vue-gtag-next";
 
 const app = createApp(App);
+
+app.use(VueGtag, {
+  property: { id: "G-C95Q6RBFHH" }
+});
 
 app.use(router).mount("#app");
