679357020ab9cc2216acd2459815afc2021e4a5e
Tom Claes <tomclaes@Toms-MacBook-Air.local>
2021-02-08 13:42:44 +0100

    pwa icons fixed

---
+++ dist/precache-manifest.38fdbb9f1a0c10e8dee5f30e6daf9c55.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5eec73d64d49f5dbed7f",
+    "url": "/css/app.44f2c271.css"
+  },
+  {
+    "revision": "f5494806f76595412b9cadd59b0d3597",
+    "url": "/fonts/LittleDinosaur-Bold.f5494806.otf"
+  },
+  {
+    "revision": "199b312db5196faf41a08f1df5dbe99f",
+    "url": "/fonts/LittleDinosaur.199b312d.otf"
+  },
+  {
+    "revision": "96aa18e7f5d2be90cb931972abb42010",
+    "url": "/img/patchi.96aa18e7.svg"
+  },
+  {
+    "revision": "8c6726356159269bd468fc80033b9a6e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5eec73d64d49f5dbed7f",
+    "url": "/js/app.82a64134.js"
+  },
+  {
+    "revision": "69e0c6faeccebf92f8d9",
+    "url": "/js/chunk-vendors.9a8623b3.js"
+  },
+  {
+    "revision": "e356f6dff0d7181e675985d921292b08",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
