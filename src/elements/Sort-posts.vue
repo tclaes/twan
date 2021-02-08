1532f3a9dfbf6f7eb8ea1a21fc95eef1d2397118
Tom Claes <tomclaes@Toms-MacBook-Air.local>
2021-02-08 13:49:00 +0100

    pwa icons

---
+++ src/elements/Sort-posts.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <select name="posts" id="posts" @change="onChange">
+      <option value="first" default selected>Oudste bericht eerst</option>
+      <option value="last">Nieuwste bericht eerst</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    onChange() {
+      this.$store.commit('reversePosts')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  select {
+    padding: .5rem;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const { createApp } = require('vue');
+import { createStore } from 'vuex'
 import App from "./App.vue";
 
 import './assets/css/styles.css';
@@ -8,8 +9,25 @@ import VueGtag from "vue-gtag-next";
 
 const app = createApp(App);
 
+const store = createStore({
+  state() {
+    return {
+      posts: {}
+    }
+  },
+  mutations: {
+    setPosts (state, payload) {
+      state.posts = payload;
+    },
+    reversePosts (state) {
+      state.posts = state.posts.reverse();
+    }
+  }
+});
+
 app.use(VueGtag, {
   property: { id: "G-C95Q6RBFHH" }
 });
 
+app.use(store);
 app.use(router).mount("#app");
