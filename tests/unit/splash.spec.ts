83bd0e545f65e01981bd78d3496aee29bc292c4d
Tom Claes <tom.claes82@gmail.com>
2021-05-11 22:06:50 +0200

    Feature/image gallery (#16)
    * tests fixed by updating vue
    
    * img gallery component added
    
    * img gallery component added
    
    * img gallery component added

--- tests/unit/splash.spec.ts
+++ tests/unit/splash.spec.ts
@@ -1,20 +1,19 @@
-import { mount } from '@vue/test-utils';
-import Splash from '@/components/Splash.vue';
-import Dinosaur from '@/elements/Dinosaur.vue';
-import Twan from '@/elements/Twan.vue';
-import Down from '@/elements/Down.vue';
+import { mount } from "@vue/test-utils";
+import Splash from "@/components/Splash.vue";
+import Dinosaur from "@/elements/Dinosaur.vue";
+import Twan from "@/elements/Twan.vue";
+import Down from "@/elements/Down.vue";
 
-describe('Splash Test', () => {
-  
-  it('should contain components', async () => {
+describe("Splash Test", () => {
+  it("should contain components", async () => {
     // render the component
-    const wrapper = mount(Splash)
+    const wrapper = mount(Splash);
 
     expect(wrapper.exists()).toBe(true);
 
     // check the components
-    expect(wrapper.getComponent(Dinosaur))
-    expect(wrapper.getComponent(Twan))
-    expect(wrapper.getComponent(Down))
-  })
-})
+    expect(wrapper.getComponent(Dinosaur));
+    expect(wrapper.getComponent(Twan));
+    expect(wrapper.getComponent(Down));
+  });
+});
--- tests/unit/twan.spec.ts
+++ tests/unit/twan.spec.ts
@@ -1,21 +1,20 @@
-import { mount } from '@vue/test-utils';
-import Twan from '@/elements/Twan.vue';
+import { mount } from "@vue/test-utils";
+import Twan from "@/elements/Twan.vue";
 
-describe('Splash Test', () => {
-  
-  it('should contain components', async () => {
+describe("Splash Test", () => {
+  it("should contain components", async () => {
     // render the component
-    const wrapper = mount(Twan)
+    const wrapper = mount(Twan);
 
     expect(wrapper.exists()).toBe(true);
-    expect(wrapper.attributes().id).toBe('twan')
-    
-    const h1 = wrapper.find('h1');
-    expect(h1.exists()).toBe(true)
-    expect(h1.classes('uppercase')).toBe(true)
-    
-    const h2 = wrapper.find('h2');
-    expect(h2.exists()).toBe(true)
-    expect(h2.classes('uppercase')).toBe(true)
-  })
-})
+    expect(wrapper.attributes().id).toBe("twan");
+
+    const h1 = wrapper.find("h1");
+    expect(h1.exists()).toBe(true);
+    expect(h1.classes("uppercase")).toBe(true);
+
+    const h2 = wrapper.find("h2");
+    expect(h2.exists()).toBe(true);
+    expect(h2.classes("uppercase")).toBe(true);
+  });
+});
