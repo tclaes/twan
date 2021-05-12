fa981e25e4681f876bddf49ed96cc7568bb546d9
Tom <tom.claes82@gmail.com>
2021-05-12 21:48:08 +0200

    temp fix landscape styling

--- src/components/Splash.vue
+++ src/components/Splash.vue
@@ -1,48 +1,53 @@
 <template>
-	<div class="splash">
-		<Dinosaur />
-		<Twan />
-		<Down color="#fff" />
-	</div>
+  <div class="splash">
+    <Dinosaur />
+    <Twan />
+    <Down color="#fff" />
+  </div>
 </template>
 
 <script>
-	import Dinosaur from './../elements/Dinosaur.vue';
-	import Twan from './../elements/Twan.vue';
-	import Down from './../elements/Down.vue';
+import Dinosaur from "./../elements/Dinosaur.vue";
+import Twan from "./../elements/Twan.vue";
+import Down from "./../elements/Down.vue";
 
 export default {
   name: "Splash",
   components: {
-		Dinosaur,
-		Twan,
-		Down
-	},
-	mounted: function(){
-		let vh = window.innerHeight * 0.01;
-		document.documentElement.style.setProperty('--vh', `${vh}px`);
-		
-		window.addEventListener('resize', () => {
-			let vh = window.innerHeight * 0.01;
-			document.documentElement.style.setProperty('--vh', `${vh}px`);
-		});
-	}
+    Dinosaur,
+    Twan,
+    Down
+  },
+  mounted: function() {
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  }
 };
 </script>
 
-<style scoped>
-	.splash {
-		background: #4ea888;
-		display: grid;
-		grid-template-rows: 1fr 2fr 100px;
-		place-items: center;
-		height: 100vh;
-		height: calc(var(--vh, 1vh) * 100);
-		margin: 0 0 2rem;
-	}
+<style scoped lang="scss">
+.splash {
+  background: #4ea888;
+  display: grid;
+  grid-template-rows: 1fr 2fr 100px;
+  place-items: center;
+  margin: 0 0 2rem;
+}
 
-	#twan {
-		align-self: end;
-		margin-bottom: 5rem;
-	}
+@media (orientation: portrait) {
+  .splash {
+    height: 100vh;
+    height: calc(var(--vh, 1vh) * 100);
+  }
+}
+
+#twan {
+  align-self: end;
+  margin-bottom: 5rem;
+}
 </style>
--- src/elements/Dinosaur.vue
+++ src/elements/Dinosaur.vue
@@ -1,26 +1,31 @@
 <template>
-	<div id="dinosaur">
-		<img src="./../assets/patchi.svg" alt="Patchi" >
-	</div>
+  <div id="dinosaur">
+    <img src="./../assets/patchi.svg" alt="Patchi" />
+  </div>
 </template>
 
 <script>
-	export default {
+export default {
   name: "Dinosaur"
 };
 </script>
 
 <style scoped>
-  #dinosaur {
-    display: flex;
-    justify-content: center;
-    margin-top: 2rem;
-    width: 100%;
-  }
+#dinosaur {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+}
 
-  img {
-    display: flex;
-    flex: 0 1 300px;
-    padding: 2rem;
+img {
+  display: flex;
+  flex: 0 1 300px;
+  padding: 2rem;
+}
+
+@media screen and (min-width: 724px) {
+  #dinosaur {
+    margin-top: 2rem;
   }
+}
 </style>
