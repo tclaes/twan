6af6e5a2417754c055bd716ed1c92c62ad1dd62d
Tom <tom.claes82@gmail.com>
2021-05-22 22:11:15 +0200

    waarom

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="navigation">
     <ul>
       <li><router-link to="/">Home</router-link></li>
-      <li><router-link to="about">About</router-link></li>
+      <li><router-link to="about">Waarom</router-link></li>
     </ul>
   </nav>
 </template>
