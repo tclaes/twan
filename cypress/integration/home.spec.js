7f3ee4944d9c7646d818ae80446f5e6da3480c3b
tclaes <tom.claes82@gmail.com>
2021-07-05 12:16:25 +0200

    Cypress added

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+context('Home', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:8081')
+  })
+
+  it('.scrollIntoView() - scroll an element into view', () => {
+
+    cy.get('.splash a').should('be.visible')
+    cy.get('.splash a').click()
+
+    cy.get('#twan-storie').should('be.visible')
+  })
+
+  it('.select() - select an option in a <select> element', () => {
+    cy.get('.splash a').click()
+
+    // at first, no option should be selected
+    cy.get('select#posts option:selected').should('have.text', 'Nieuwste bericht eerst')
+  })
+})
